53a9cb9779b824f90fbcba6250c610ecbe7c061e
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2020-04-28 19:59:47 +0300

    created the lyrics page by making a request to the Musixmatch API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from './context';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 
-import { Provider } from './context';
+import Lyrics from './components/tracks/Lyrics';
 
 import './App.css';
 
@@ -18,6 +19,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
